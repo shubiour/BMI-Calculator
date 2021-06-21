3a54fe3f291cfac95dadc2678eb034a921da1e96
shubiour <sv95.shuvo@gmail.com>
2021-06-21 19:40:59 +0600

    bmi calclutor

--- lib/screens/CalculatorBrain.dart
+++ lib/screens/CalculatorBrain.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 import '../assets/constants.dart';
+
 class CalculatorBrain {
   final int? height;
   final int? weight;
@@ -26,7 +27,7 @@ class CalculatorBrain {
 
   String getInerpretation() {
     if (_bmi! >= 18.5 && _bmi! <= 22.9) {
-      return " You have a normal weight ";
+      return " You have a normal weight. ";
     } else if (_bmi! >= 23) {
       return " You have a higher than normal body weight. Try to exercise more. ";
     } else {
@@ -34,19 +35,32 @@ class CalculatorBrain {
     }
   }
 
-
   List getTips() {
     if (_bmi! >= 18.5 && _bmi! <= 22.9) {
-      return ["Good job! "];
-    }
-    if (_bmi! >= 23) {
+      return [
+        "Good",
+        "job",
+        "!",
+        kBodyTextStyle,
+      ];
+    } else if (_bmi! >= 23) {
       double _newbmi = _bmi! - 23;
       double _newWeight = (_newbmi * pow(height! / 100, 2));
-      return [ "💡 Tips: Try to loose ",_newWeight.toStringAsFixed(2) , " kg",kOverWeightTextStyle];
+      return [
+        "💡 Tips: Try to loose ",
+        _newWeight.toStringAsFixed(2),
+        " kg",
+        kOverWeightTextStyle
+      ];
     } else {
       double _newbmi = 18.5 - _bmi!;
       double _newWeight = (_newbmi * pow(height! / 100, 2));
-      return ["💡 Tips: Try to gain ",_newWeight.toStringAsFixed(2) , " kg",kUnderrWeightTextStyle];
+      return [
+        "💡 Tips: Try to gain ",
+        _newWeight.toStringAsFixed(2),
+        " kg",
+        kUnderrWeightTextStyle
+      ];
     }
   }
 }
